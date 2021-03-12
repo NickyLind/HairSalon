e9271ade227c3c266a70743957f5c11907105cbe
Nick Lindau <nicholaithegreat@gmail.com>
2021-03-12 13:20:01 -0800

    fix redirect in client controller to the stylist index

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -32,7 +32,7 @@ namespace HairSalon.Controllers
     {
       _db.Clients.Add(client);
       _db.SaveChanges();
-      return RedirectToAction("Index");
+      return RedirectToAction("Index", "Stylists");
     }
 
     public ActionResult Details(int id)
